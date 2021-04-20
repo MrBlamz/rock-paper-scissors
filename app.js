7adacbd3ca903db7feaa914fd49f369a480c8910
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-04-20 18:31:07 +0100

    Add function that plays a single round

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const Hands = ["Rock", "Paper", "Scissors"];
+const HANDS = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
-  return Hands[Math.round(Math.random() * 2)];
+  return HANDS[Math.round(Math.random() * 2)];
 }
 
 function playerPlay() {
   // Ask player for a hand //
   function getPlayerHand() {
-    return prompt(
+    let input = prompt(
       "Please choose one the following hands:",
       "Rock, Paper, Scissors"
     );
+
+    if (input === null) {
+      throw "Canceled! Stopping execution!";
+    } else {
+      return input;
+    }
   }
 
   // Sanitizes player input //
@@ -22,7 +28,7 @@ function playerPlay() {
 
   // Check if user hand is valid (returns a boolean) //
   function isValidHand(hand) {
-    return Hands.includes(hand);
+    return HANDS.includes(hand);
   }
 
   while (!isValidHand(hand)) {
@@ -34,7 +40,31 @@ function playerPlay() {
   return hand;
 }
 
-// TODO
-function round(playerSelection, computerSelection) {}
-
-console.log(playerPlay());
+function round(playerSelection, computerSelection) {
+  switch (playerSelection) {
+    case "Rock":
+      if (computerSelection === "Rock") {
+        return "This round ended in a tie!";
+      } else if (computerSelection === "Paper") {
+        return "You lose! Paper beats Rock";
+      } else {
+        return "You win! Rock beats Scissors";
+      }
+    case "Paper":
+      if (computerSelection === "Rock") {
+        return "You win! Paper beats Rock";
+      } else if (computerSelection === "Paper") {
+        return "This round ended in a tie!";
+      } else {
+        return "You lose! Scissors beats Paper";
+      }
+    case "Scissors":
+      if (computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors";
+      } else if (computerSelection === "Paper") {
+        return "You win! Scissors beats Paper";
+      } else {
+        return "This round ended in a tie!";
+      }
+  }
+}
