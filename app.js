b6b5fc8e7368e345cfb9af735f592e87a6b1dbc6
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-04-20 16:09:11 +0100

    Add function that asks player for a hand

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-function computerPlay() {
-  const CHOICES = ["Rock", "Paper", "Scissors"];
+const Hands = ["Rock", "Paper", "Scissors"];
 
-  return CHOICES[Math.round(Math.random() * 2)];
+function computerPlay() {
+  return Hands[Math.round(Math.random() * 2)];
 }
+
+function playerPlay() {
+  // Ask player for a hand //
+  function getPlayerHand() {
+    return prompt(
+      "Please choose one the following hands:",
+      "Rock, Paper, Scissors"
+    );
+  }
+
+  // Sanitizes player input //
+  function sanitizeInput(input) {
+    return input[0].toUpperCase() + input.slice(1).toLowerCase();
+  }
+
+  let hand = sanitizeInput(getPlayerHand());
+
+  // Check if user hand is valid (returns a boolean) //
+  function isValidHand(hand) {
+    return Hands.includes(hand);
+  }
+
+  while (!isValidHand(hand)) {
+    alert("Please choose a valid hand!");
+
+    hand = sanitizeInput(getPlayerHand());
+  }
+
+  return hand;
+}
+
+// TODO
+function round(playerSelection, computerSelection) {}
+
+console.log(playerPlay());
